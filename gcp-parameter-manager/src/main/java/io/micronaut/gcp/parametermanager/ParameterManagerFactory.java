ef4d6b8dc9eea795a08aa45c4ebbd131a867d5f0
abheda-crest <alfatah.bheda@crestdata.ai>
2025-11-26 15:57:53 +0530

    added parameter manager client factory

---
+++ gcp-parameter-manager/src/main/java/io/micronaut/gcp/parametermanager/ParameterManagerFactory.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2017-2025 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.gcp.parametermanager;
+
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.api.gax.grpc.InstantiatingGrpcChannelProvider;
+import com.google.api.gax.rpc.TransportChannelProvider;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.parametermanager.v1.ParameterManagerClient;
+import com.google.cloud.parametermanager.v1.ParameterManagerSettings;
+import io.micronaut.context.annotation.BootstrapContextCompatible;
+import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.gcp.Modules;
+import io.micronaut.gcp.UserAgentHeaderProvider;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+
+import java.io.IOException;
+
+/**
+ * Factory to create ParameterManager clients.
+ *
+ * @author Alfatah Bheda
+ * @since 6.0.0
+ */
+@Factory
+@Requires(classes = {ParameterManagerClient.class})
+@BootstrapContextCompatible
+public class ParameterManagerFactory {
+
+    /**
+     * Creates a {@link ParameterManagerClient} instance.
+     *
+     * @param credentialsProvider      - Google Cloud Credentials Provider
+     * @param transportChannelProvider - Transport Channel Provider
+     * @return an instance using defaults.
+     */
+    @Singleton
+    public ParameterManagerClient parameterManagerClient(@Named(Modules.PARAMETER_MANAGER) CredentialsProvider credentialsProvider, @Named(Modules.PARAMETER_MANAGER) TransportChannelProvider transportChannelProvider) {
+        try {
+            ParameterManagerSettings.Builder builder = ParameterManagerSettings.newBuilder();
+            ParameterManagerSettings settings = builder.setCredentialsProvider(credentialsProvider).setTransportChannelProvider(transportChannelProvider).build();
+
+            return ParameterManagerClient.create(settings);
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not instantiate ParameterManagerClient", e);
+        }
+    }
+
+    /**
+     * Returns a default {@link CredentialsProvider}, allows users to override it and provide their own implementation.
+     *
+     * @param credentials default credentials, if not overridden by user should be provided by {@link io.micronaut.gcp.credentials.GoogleCredentialsFactory}
+     * @return A {@link FixedCredentialsProvider} holding the given credentials.
+     */
+    @Singleton
+    @Named(Modules.PARAMETER_MANAGER)
+    public CredentialsProvider credentialsProvider(GoogleCredentials credentials) throws IOException {
+        return FixedCredentialsProvider.create(credentials);
+    }
+
+    /**
+     * @return default {@link TransportChannelProvider} TransportChannelProvider
+     */
+    @Singleton
+    @Named(Modules.PARAMETER_MANAGER)
+    public TransportChannelProvider transportChannelProvider() {
+        return InstantiatingGrpcChannelProvider.newBuilder().setHeaderProvider(new UserAgentHeaderProvider(Modules.PARAMETER_MANAGER)).build();
+    }
+}
